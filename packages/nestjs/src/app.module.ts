d81596a2165043e5e679ff405c343cbc187e5287
Jeremy Zaborowski <jeremy@dayslice.io>
2023-09-20 20:37:06 -0400

    [feat] Added support for Vectors in DB using pgvector [feat] working nestJS using Typeorm

--- packages/nestjs/src/app.module.ts
+++ packages/nestjs/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EntitiesModule } from './entities/entities.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import connectionOptions from './ormconfig';
 
+console.log('ORM', connectionOptions);
 @Module({
-  imports: [EntitiesModule],
+  imports: [TypeOrmModule.forRoot(connectionOptions), EntitiesModule],
   controllers: [AppController],
   providers: [AppService],
 })

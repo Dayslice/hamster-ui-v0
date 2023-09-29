333676e9283f5cd9bb4ec29e96804cebabd555d4
Jeremy Zaborowski <jeremy@dayslice.io>
2023-09-29 10:48:02 -0400

    [fix] use netlify URL

--- packages/sveltekit/src/lib/utils/fetchLogs.ts
+++ packages/sveltekit/src/lib/utils/fetchLogs.ts
@@ -2,7 +2,7 @@ import type { LogData } from '../../routes/logger/logger.types';
 
 // src/lib/apiUtils.ts
 export async function fetchLogs(): Promise<LogData[]> {
-  const response = await fetch(`http://localhost:3001/logs`);
+  const response = await fetch(`https://preeminent-crostata-e1435d.netlify.app/logs`);
   if (!response.ok) {
     throw new Error(`HTTP error! Status: ${response.status}`);
   }

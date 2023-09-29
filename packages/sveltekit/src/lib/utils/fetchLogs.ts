7217d5224e9f8019f6edd5f217172ae199740e5e
Jeremy Zaborowski <jeremy@dayslice.io>
2023-09-29 14:07:28 -0400

    [fix] using DEV to change the API URL

--- packages/sveltekit/src/lib/utils/fetchLogs.ts
+++ packages/sveltekit/src/lib/utils/fetchLogs.ts
@@ -1,7 +1,8 @@
 import type { LogData } from '../../routes/logger/logger.types';
 // src/lib/apiUtils.ts
 export async function fetchLogs(): Promise<LogData[]> {
-  const response = await fetch(`http://localhost:3001/logs`);
+  const url = import.meta.env.DEV ? 'http://localhost:3001' : 'https://preeminent-crostata-e1435d.netlify.app/';
+  const response = await fetch(`${url}/logs`);
   if (!response.ok) {
     throw new Error(`HTTP error! Status: ${response.status}`);
   }

275a6b81c92736f05ef4616091aa2d9b662c7a8c
Jeremy Zaborowski <jeremy@dayslice.io>
2023-10-16 14:37:07 -0400

    [fix] 501 is not a new page just replaces the page content instead

--- packages/sveltekit/src/routes/(private)/+layout.svelte
+++ packages/sveltekit/src/routes/(private)/+layout.svelte
@@ -3,15 +3,13 @@
   import { page } from '$app/stores';
   import Content from '$lib/layout/Content.svelte';
   import LeftNav from '$lib/layout/LeftNav.svelte';
+  import Panel from '$lib/layout/Panel.svelte';
 
   import state from '$lib/stores/state.store';
   import { onMount } from 'svelte';
 
   onMount(() => {
     $state.admin = window.localStorage.getItem('admin') ? true : false;
-    if ($state.admin == false || $page.url.searchParams.has('admin')) {
-      goto('/501');
-    }
   });
 </script>
 
@@ -20,4 +18,9 @@
   <Content>
     <slot />
   </Content>
+{:else}
+  <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div class="text-4xl font-semibold mb-4 text-gray-700">501 - Unauthorized</div>
+    <Panel class="text-2xl font-medium text-white !bg-red-500 p-6 rounded-lg shadow-lg">Unauthorized Access</Panel>
+  </div>
 {/if}

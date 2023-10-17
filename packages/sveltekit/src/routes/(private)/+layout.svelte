b0ed23ab51ec2798bf0801cf0b736f53bc7873ad
Jeremy Zaborowski <jeremy@dayslice.io>
2023-10-16 20:07:36 -0400

    Dont show the 501 until the localstorage is loaded

--- packages/sveltekit/src/routes/(private)/+layout.svelte
+++ packages/sveltekit/src/routes/(private)/+layout.svelte
@@ -1,6 +1,4 @@
 <script>
-  import { goto } from '$app/navigation';
-  import { page } from '$app/stores';
   import Content from '$lib/layout/Content.svelte';
   import LeftNav from '$lib/layout/LeftNav.svelte';
   import Panel from '$lib/layout/Panel.svelte';
@@ -8,19 +6,23 @@
   import state from '$lib/stores/state.store';
   import { onMount } from 'svelte';
 
+  let loaded = false;
   onMount(() => {
     $state.admin = window.localStorage.getItem('admin') ? true : false;
+    loaded = true;
   });
 </script>
 
-{#if $state.admin}
-  <LeftNav />
-  <Content>
-    <slot />
-  </Content>
-{:else}
-  <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
-    <div class="text-4xl font-semibold mb-4 text-gray-700">501 - Unauthorized</div>
-    <Panel class="text-2xl font-medium text-white !bg-red-500 p-6 rounded-lg shadow-lg">Unauthorized Access</Panel>
-  </div>
+{#if loaded}
+  {#if $state.admin}
+    <LeftNav />
+    <Content>
+      <slot />
+    </Content>
+  {:else}
+    <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <div class="text-4xl font-semibold mb-4 text-gray-700">501 - Unauthorized</div>
+      <Panel class="text-2xl font-medium text-white !bg-red-500 p-6 rounded-lg shadow-lg">Unauthorized Access</Panel>
+    </div>
+  {/if}
 {/if}
